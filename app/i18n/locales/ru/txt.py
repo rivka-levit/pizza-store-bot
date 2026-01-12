c28b8f4b780f689929efd98e3d2f76448e42681c
rivka-yuliia <rivka.developer@gmail.com>
2026-01-12 18:22:14 +0200

    marked list text and emoji ❗

--- app/i18n/locales/ru/txt.py
+++ app/i18n/locales/ru/txt.py
@@ -1,12 +1,35 @@
 from typing import Any
 
+from aiogram.utils.formatting import as_list, as_marked_section, Bold
+
 RU: dict[str, str | Any] = {
-    '/start': 'Привет, я виртуальный помощник.',
-    '/help': 'Бот помогает заказать пиццу.',
-    '/menu': 'Вот меню:',
-    '/about': 'О нас:',
-    '/payment': 'Варианты оплаты:',
-    '/shipping': 'Варианты доставки:',
+    '/start': 'Привет, я виртуальный помощник 👨‍💻',
+    '/help': 'Бот помогает заказать пиццу 🍕',
+    '/menu': '<b>Вот меню:</b>',
+    '/about': '<b>О нас:</b>',
+    '/payment': as_marked_section(
+        Bold('Варианты оплаты:'),
+        'Картой в боте',
+        'При получении карта/кэш',
+        'В заведении',
+        marker='✅ '
+    ).as_html(),
+    '/shipping': as_list(
+        as_marked_section(
+            Bold('Варианты доставки/заказа:'),
+            'Курьер',
+            'Самовывоз (сейчас прибегу заберу)',
+            'Покушаю у Вас (сейчас прибегу)',
+            marker='✅ '
+        ),
+        as_marked_section(
+            Bold('Нельзя:'),
+            'Почта',
+            'Голуби',
+            marker='❌ '
+        ),
+        sep='\n------------------\n'
+    ).as_html(),
     'start_description': 'Перезапустить бота',
     'help_description': 'Справка по работе бота',
     'menu_description': 'Меню пиццерии',
