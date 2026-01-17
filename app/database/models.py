5e0b9e5305435a70ba2ebc047ad5be7263799f58
rivka-yuliia <rivka.developer@gmail.com>
2026-01-17 20:41:05 +0200

    product model for db

---
+++ app/database/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import DateTime, Float, String, Text, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+    updated: Mapped[DateTime] = mapped_column(
+        DateTime,
+        default=func.now(),
+        onupdate=func.now()
+    )
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    description: Mapped[str] = mapped_column(Text(500))
+    price: Mapped[float] = mapped_column(Float(asdecimal=True), nullable=False)
+    image: Mapped[str] = mapped_column(String(150))
