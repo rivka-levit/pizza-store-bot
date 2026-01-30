dde7f90526f0564f50e02d2a0d7c3b77d614c328
rivka-yuliia <rivka.developer@gmail.com>
2026-01-30 14:54:20 +0200

    cart model and relationships

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import DateTime, ForeignKey, Numeric, String, Text, func
+from typing import Set
+
+from sqlalchemy import DateTime, Integer, ForeignKey, Numeric, String, Text, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
@@ -12,9 +14,9 @@ class Base(DeclarativeBase):
 
 
 class Banner(Base):
-    __tablename__ = 'banners'
+    __tablename__ = 'banner'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
     image: Mapped[str] = mapped_column(String(150), nullable=True)
     description: Mapped[str] = mapped_column(Text, nullable=True)
@@ -23,30 +25,52 @@ class Banner(Base):
 class Category(Base):
     __tablename__ = 'categories'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(150), nullable=False)
+    products: Mapped[Set['Product']] = relationship('Product', back_populates='category')
 
 
 class Product(Base):
     __tablename__ = 'products'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
     price: Mapped[float] = mapped_column(Numeric(5,2), nullable=False)
     image: Mapped[str] = mapped_column(String(150))
     category_id: Mapped[int] = mapped_column(
-        ForeignKey('category.id', onupdate='CASCADE'),
+        ForeignKey('category.id', ondelete='CASCADE'),
         nullable=False
     )
-    category: Mapped[Category] = relationship(backref='products')
+    category: Mapped['Category'] = relationship('Category', back_populates='products')
+    carts: Mapped[Set['Cart']] = relationship('Cart', back_populates='product')
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     tg_id: Mapped[int] = mapped_column(unique=True, nullable=False)
     first_name: Mapped[str] = mapped_column(String(150), nullable=True)
     last_name: Mapped[str] = mapped_column(String(150), nullable=True)
     phone: Mapped[str] = mapped_column(String(13), nullable=True)
+
+    carts: Mapped[Set['Cart']] = relationship('Cart', back_populates='user')
+
+
+class Cart(Base):
+    __tablename__ = 'carts'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('user.tg_id', ondelete='CASCADE'),
+        nullable=False
+    )
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey('product.id', ondelete='CASCADE'),
+        nullable=False
+    )
+    quantity: Mapped[int] = mapped_column(Integer, default=1, nullable=False)
+
+    user: Mapped['User'] = relationship('User', back_populates='carts')
+    product: Mapped['Product'] = relationship('Product', back_populates='carts')
