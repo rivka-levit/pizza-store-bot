13b712fb0d3d79ad73d5afa528ae0cba69092fb8
rivka-yuliia <rivka.developer@gmail.com>
2026-02-01 17:51:17 +0200

    orm banners

--- app/database/orm_query.py
+++ app/database/orm_query.py
@@ -5,8 +5,46 @@ from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped
 
-from database.models import Product
+from database.models import Product, Banner
 
+
+# ===================== Banners (info pages) ============================
+
+async def orm_add_banner_description(session: AsyncSession, data: dict) -> None:
+    query = select(Banner)
+    result = await session.execute(query)
+    if result.first():
+        return
+    session.add_all(
+        [Banner(name=name, description=descr) for name, descr in data.items()]
+    )
+    await session.commit()
+
+
+async def orm_change_banner_image(
+        session: AsyncSession,
+        name: str,
+        image: str
+) -> None:
+    query = update(Banner).where(Banner.name == name).values(image=image)
+    await session.execute(query)
+    await session.commit()
+
+
+async def orm_get_banner(session: AsyncSession, page_name: str) -> Banner:
+    query = select(Banner).where(Banner.name == page_name)
+    result = await session.execute(query)
+    return result.scalars().first()
+
+
+async def orm_get_info_pages(session: AsyncSession) -> Sequence[Banner]:
+    query = select(Banner)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+# =========================== Categories ================================
+
+# ========================== Admin Panel ================================
 
 async def orm_add_product(session: AsyncSession, data: dict[str, Any]) -> None:
     """Add product to database"""
